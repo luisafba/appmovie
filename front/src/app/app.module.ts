aed9b37186e66355ff1ff18e637301d238afb60e
lburgos <luisa.burgos@yopmail.com>
2019-11-13 10:39:49 -0500

    modelo de producto probado en postman

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearCapituloComponent } from './crear-capitulo/crear-capitulo.component';
+import { CrearProductoComponent } from './crear-producto/crear-producto.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrearCapituloComponent
+    CrearCapituloComponent,
+    CrearProductoComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
