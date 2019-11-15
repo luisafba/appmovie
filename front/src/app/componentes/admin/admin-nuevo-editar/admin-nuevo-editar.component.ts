96fc945e6c75d9ff229094f2ea3b2235b4a75833
paulis1996 <55591727+paulis1996@users.noreply.github.com>
2019-11-15 09:41:46 -0500

    Update admin-nuevo-editar.component.ts

--- front/src/app/componentes/admin/admin-nuevo-editar/admin-nuevo-editar.component.ts
+++ front/src/app/componentes/admin/admin-nuevo-editar/admin-nuevo-editar.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { NotificacionService } from "../../../servicios/notificacion.service";
+import { SeriesPelisService } from "../../../servicios/seriespelis.service";
 
 @Component({
-  selector: 'app-adminNuevoEditar',
-  templateUrl: './admin-nuevo-editar.component.html',
-  styleUrls: ['./admin-nuevo-editar.component.css']
+  selector: "app-adminNuevoEditar",
+  templateUrl: "./admin-nuevo-editar.component.html",
+  styleUrls: ["./admin-nuevo-editar.component.css"]
 })
 export class AdminNuevoEditarComponent implements OnInit {
+  constructor(
+    private builder: FormBuilder,
+    private router: Router,
+    private notificacionService: NotificacionService,
+    private _seriePelis: SeriesPelisService
+  ) {}
+  imageToUpload: File = null;
+  videoToUpload: File = null;
+  isLoadingResults = false;
+  editarPeliculaForm: FormGroup = this.builder.group({
+    nombre: ["", Validators.required],
+    genero: ["", Validators.required],
+    anio: ["", Validators.required],
+    duracion: [
+      "",
+      Validators.compose([Validators.required, Validators.minLength(1)])
+    ],
+    sinopsis: ["", Validators.required],
+    imagen: ["", Validators.required],
+    clasificacion: ["", Validators.required],
+    director: ["", Validators.required],
+    protagonista: ["", Validators.required],
+    archivo: [null, Validators.required],
+    comentarios: ["", Validators.required]
+  });
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  enviar() {
+    this.isLoadingResults = true;
+    console.log(this.editarPeliculaForm.value);
+    this._seriePelis
+      .postSeriePelis(this.editarPeliculaForm.value, this.imageToUpload, this.videoToUpload)
+      .subscribe(response => {
+        const id = response._id;
+        this.isLoadingResults = false;
+        this.router.navigate(["/home"]);
+        this.notificacionService.success(
+          "Película registrada satisfactoriamente!"
+        );
+      },
+      (err: any) => {
+        console.log(err);
+        this.notificacionService.error("Error al registrar la película");
+        this.isLoadingResults = false;
+      });
   }
 
+  handleImageInput(files: FileList) {
+    this.imageToUpload = files.item(0);
+  }
+
+  handleVideoInput(files: FileList) {
+    this.videoToUpload = files.item(0);
+  }
 }
