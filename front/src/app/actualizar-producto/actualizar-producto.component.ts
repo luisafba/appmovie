16816168c4a3231171f6ab194a304fb98061842c
paula orjuela <paulis1996nat@gmai.com>
2019-11-16 10:38:12 -0500

    cambio de botos menu

--- front/src/app/actualizar-producto/actualizar-producto.component.ts
+++ front/src/app/actualizar-producto/actualizar-producto.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-actualizar-producto',
-  templateUrl: './actualizar-producto.component.html',
-  styleUrls: ['./actualizar-producto.component.css']
+  selector: "actualizarproducto",
+  templateUrl: "./actualizar-producto.component.html",
+  styleUrls: ["./actualizar-producto.component.css"]
 })
 export class ActualizarProductoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
