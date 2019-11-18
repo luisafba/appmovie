896e1c27d010e1da867daeea44f36480cc60779b
lburgos <luisa.burgos@yopmail.com>
2019-11-17 23:22:04 -0500

    Se agregan los parametros al servicio de consulta

--- front/src/app/servicios/producto.service.ts
+++ front/src/app/servicios/producto.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of  } from "rxjs";
+import { Observable, of } from "rxjs";
 import { catchError, map, tap } from 'rxjs/operators';
 import { Producto } from "../modelos/producto.module";
 
@@ -14,73 +14,79 @@ export class ProductoService {
     //headers: new HttpHeaders({ "Accept": "application/json" })
   };
 
-readonly Url_API = 'http://localhost:3000/api/productos'
+  readonly Url_API = 'http://localhost:3000/api/productos'
 
-constructor(private http: HttpClient) { }
-  getProductos(): Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.Url_API);
+  constructor(private http: HttpClient) { }
+  getProductos(nombre?:string, genero?:string): Observable<Producto[]> {
+    let urlConsultar = this.Url_API+"?";
+    if (nombre)
+      urlConsultar += "nombre="+nombre+"&";
+    if (genero)
+      urlConsultar += "genero="+genero;
+      console.log(urlConsultar);
+    return this.http.get<Producto[]>(urlConsultar);
   }
 
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-    console.error(error); // log to console instead
-    return of(result as T);
-  };
-}
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      return of(result as T);
+    };
+  }
 
-postProducto(producto:Producto): Observable<Producto> {
-  var formData: any = new FormData();
-  formData.append("nombre", producto.nombre);
-  formData.append("genero", producto.genero);
-  formData.append("anio", producto.anio);
-  formData.append("edad", producto.edad);
-  formData.append("duracion", producto.duracion);
-  formData.append("sinopsis", producto.sinopsis);
-  formData.append("imagen", producto.imagen);
-  formData.append("clasificacion", producto.clasificacion);
-  formData.append("like", 0);
-  formData.append("dislike", 0);
-  formData.append("director", producto.director);
-  formData.append("protagonista", producto.protagonista);
-  formData.append("video", producto.video);
-  
-  return this.http.post<Producto>(this.Url_API, formData, this.httpOptions);
-}
+  postProducto(producto: Producto): Observable<Producto> {
+    var formData: any = new FormData();
+    formData.append("nombre", producto.nombre);
+    formData.append("genero", producto.genero);
+    formData.append("anio", producto.anio);
+    formData.append("edad", producto.edad);
+    formData.append("duracion", producto.duracion);
+    formData.append("sinopsis", producto.sinopsis);
+    formData.append("imagen", producto.imagen);
+    formData.append("clasificacion", producto.clasificacion);
+    formData.append("like", 0);
+    formData.append("dislike", 0);
+    formData.append("director", producto.director);
+    formData.append("protagonista", producto.protagonista);
+    formData.append("video", producto.video);
 
-getProducto(id: number): Observable<Producto> {
-  const url = this.Url_API+"/"+id;
-  console.log(url);
-  return this.http.get<Producto>(url).pipe(
-    tap(_ => console.log('fetched Producto id='+id)),
-    catchError(this.handleError<Producto>('getProducto id='+id))
-  );
-}
+    return this.http.post<Producto>(this.Url_API, formData, this.httpOptions);
+  }
 
-updateProducto(_id:String, producto:Producto): Observable<Producto> {
-  var formData: any = new FormData();
-  const url = this.Url_API+"/"+_id;
-  formData.append("nombre", producto.nombre);
-  formData.append("genero", producto.genero);
-  formData.append("anio", producto.anio);
-  formData.append("duracion", producto.duracion);
-  formData.append("sinopsis", producto.sinopsis);
-  formData.append("imagen", producto.imagen);
-  formData.append("clasificacion", producto.clasificacion);
-  formData.append("like", producto.like);
-  formData.append("dislike", producto.dislike);
-  formData.append("director", producto.director);
-  formData.append("protagonista", producto.protagonista);
-  formData.append("video", producto.video);
-  return this.http.put<Producto>(url, formData, this.httpOptions);
-}
+  getProducto(id: number): Observable<Producto> {
+    const url = this.Url_API + "/" + id;
+    console.log(url);
+    return this.http.get<Producto>(url).pipe(
+      tap(_ => console.log('fetched Producto id=' + id)),
+      catchError(this.handleError<Producto>('getProducto id=' + id))
+    );
+  }
 
-deleteProducto(id: any): Observable<Producto> {
-  const url = this.Url_API+"/"+id;
-  return this.http.delete<Producto>(url, this.httpOptions).pipe(
-    tap(_ => console.log('deleted Producto id='+id)),
-    catchError(this.handleError<Producto>('deleteProducto'))
-  );
-}
+  updateProducto(_id: String, producto: Producto): Observable<Producto> {
+    var formData: any = new FormData();
+    const url = this.Url_API + "/" + _id;
+    formData.append("nombre", producto.nombre);
+    formData.append("genero", producto.genero);
+    formData.append("anio", producto.anio);
+    formData.append("duracion", producto.duracion);
+    formData.append("sinopsis", producto.sinopsis);
+    formData.append("imagen", producto.imagen);
+    formData.append("clasificacion", producto.clasificacion);
+    formData.append("like", producto.like);
+    formData.append("dislike", producto.dislike);
+    formData.append("director", producto.director);
+    formData.append("protagonista", producto.protagonista);
+    formData.append("video", producto.video);
+    return this.http.put<Producto>(url, formData, this.httpOptions);
+  }
+
+  deleteProducto(id: any): Observable<Producto> {
+    const url = this.Url_API + "/" + id;
+    return this.http.delete<Producto>(url, this.httpOptions).pipe(
+      tap(_ => console.log('deleted Producto id=' + id)),
+      catchError(this.handleError<Producto>('deleteProducto'))
+    );
+  }
 
 }
 
